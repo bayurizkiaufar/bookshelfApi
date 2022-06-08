f55f2cdaa1bc6e6902a8b26fb064ffed17d3d9d4
bayurizkiaufar <bayurizkiaufar@gmail.com>
2022-06-08 15:50:15 +0700

    commit

---
+++ bookshelf-api/src/books.js
@@ -0,0 +1,6 @@
+/* eslint-disable linebreak-style */
+const books = [
+
+];
+
+module.exports = books;
---
+++ bookshelf-api/src/routes.js
@@ -0,0 +1,36 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
+const handler = require('./handler');
+const {
+  addBookHandler, getAllBookHandler, getBookByIdHandler, editBookByIdHandler, deleteBookByIdHandler,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{bookId}',
+    handler: getBookByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{bookId}',
+    handler: editBookByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{bookId}',
+    handler: deleteBookByIdHandler,
+  },
+];
+
+module.exports = routes;
